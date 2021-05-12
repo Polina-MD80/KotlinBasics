b3f3160be9f09530608cda61925090012fe272dc
Polina Pavlova <poli.paskaleva@gmail.com>
2021-05-12 21:45:47 +0300

    some Kotlin ex

---
+++ Inheritance/src/academy/learnprogramming/inheritance/Printer.kt
@@ -0,0 +1,23 @@
+package academy.learnprogramming.inheritance
+
+fun main() {
+    val laserPrinter = SpecialLaserPrinter("Bora 1234")
+    println(laserPrinter.modelName)
+    laserPrinter.printModel()
+    println(laserPrinter.bestSellingPrice())
+}
+abstract class Printer(val modelName: String) {
+    open fun printModel() = println("The model name of this printer is $modelName")
+    abstract fun bestSellingPrice() : Double
+}
+
+open class LaserPrinter(modelName: String) : Printer(modelName) {
+
+  final override fun printModel() = println("The model name of this laser printer is $modelName")
+    override fun bestSellingPrice(): Double = 129.29
+}
+class SpecialLaserPrinter(modelName: String): LaserPrinter(modelName){
+    override fun bestSellingPrice(): Double {
+        return 123.34
+    }
+}
