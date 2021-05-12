3b1e660bbdca4ec5d82567417fa6ad33bd39187a
Polina Pavlova <poli.paskaleva@gmail.com>
2021-05-12 14:14:28 +0300

    some Kotlin ex

--- Functions/src/academy/programming/functions/Utils.kt
+++ Functions/src/academy/programming/functions/Utils.kt
@@ -2,11 +2,11 @@ package academy.programming.functions
 
 import java.util.*
 
-class Utils{
+class Utils {
 
-    fun upperFirstAndLast(str: String):String{
-   val  firstToUpper = str.substring(0, 1).uppercase(Locale.getDefault()) + str.substring(1)
-        return firstToUpper.substring(0, firstToUpper.length-1) + firstToUpper.substring(str.length-1)
+    fun upperFirstAndLast(str: String): String {
+        val firstToUpper = str.substring(0, 1).uppercase(Locale.getDefault()) + str.substring(1)
+        return firstToUpper.substring(0, firstToUpper.length - 1) + firstToUpper.substring(str.length - 1)
             .uppercase(Locale.getDefault())
     }
 
